3089f9522371f576ea40c78f04bc1347255411bb
jiheum <ajjarago@naver.com>
2026-06-10 12:21:52 +0900

    -record dql 데이터질의어 정의 as, distanct(제외), 산술연산, concat(스트링포멧)

--- 02_dql_select_1.sql
+++ 02_dql_select_1.sql
@@ -43,12 +43,54 @@
         제한조건을 만족시키는 행들만 Result Set에 포함됨
     4. ORDER BY : 정렬할 컬럼을 기준으로 오름/내림차순 지정
     5. GROUP BY : 행을 그룹핑함
-    6. HAVING : 그룹핑된 행을 선택하는 조건을 기술함
+    6. HAVING : 그룹핑된 행을 선택하는 조건을 기술함의
 */
 
+select * from tbl_menu;
 
+select
+    t.menu_code  as '메뉴코드',
+    t.menu_price as '가격'
+from tbl_menu as t;
 
+# 조회되는 컬럼의 연산 사능
+# 메뉴 테이블에서 메뉴명, 가격, 부가세(10%), 부가세 포함 가격
 
+# ===============================================
+# ResultSet : select 조회 결고 행의 집합
+# 컬럼명 as 별칭 : resultSet에 표기 되는 컬럼명의 별칭(alias) 가능
+# ===============================================
+select
+    menu.menu_name                       as 메뉴명,
+    menu.menu_price                      as "메뉴 가격",
+    menu.menu_price * 0.1                    부가세, -- as 생략
+    menu.menu_price + (menu_price * 0.1) as "부가세포함 가격"
+from tbl_menu as menu;
+
+# 산술연산
+select
+    10 + 3,
+    10 - 3,
+    10 * 3,
+    10 / 3,
+    10 div 3, -- 몫 구하기
+    10 % 3,
+    10 mod 3 -- 나머지 구하기
+;
+
+# 문자열 연결 처리
+# 'abc' + 'def' = 'abcdef' -- 안됨
+# concat('abc',',','def') = 'abc,def'
+# 메뉴 테이블에서 메뉴명, 메뉴가격(원)
+select
+    menu.menu_name                       as 메뉴명,
+    concat(menu.menu_price, '원')       as "메뉴 가격(원)"
+from tbl_menu as menu;
+
+# DISTINCT 컬럼값 중복 제거
+# 지정된 컬럼값의 중복값의 컬럼을 제거
+select m.category_code from tbl_menu as m;
+select distinct m.category_code from tbl_menu as m;
 
 
 
