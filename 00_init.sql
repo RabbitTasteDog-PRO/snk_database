1625fe9ece3e6b661881eea41847ee1a0887c254
jiheum <ajjarago@naver.com>
2026-06-10 10:06:47 +0900

    homebrew sql8.0제거 도커로 컨테이너 추가해서 프로젝트 진행

--- 00_init.sql
+++ 00_init.sql
@@ -5,3 +5,34 @@ create user skn_ai@'%' identified by '1234';
 create database menudb;
 
 grant all privileges on menudb.* to skn_ai@'%';
+
+show
+databases;
+
+select user(), current_user();
+
+# 도커 실행
+# open -a Docker
+
+# 도커 연결 확인
+# docker ps
+
+# root 계정 연결
+# docker exec -it mysql8 mysql -u root -p
+# pw : 1234
+
+# 사용자 계정 연결
+# docker exec -it mysql8 mysql -u skn_ai -p
+# pw : 1234
+
+
+# DB 연결
+/*
+docker run --name mysql8 \
+  -e MYSQL_ROOT_PASSWORD=1234 \
+  -e MYSQL_DATABASE=menudb \
+  -e MYSQL_USER=skn_ai \
+  -e MYSQL_PASSWORD=1234 \
+  -p 3306:3306 \
+  -d mysql:8.0
+*/
